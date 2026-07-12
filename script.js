cc506fb0f62e802aa4c9fda409b950aa2da506ce
sarthak8801-arch <sarthak8801@gmail.com>
2026-07-12 21:51:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1159,6 +1159,19 @@ Avoid the moving friends!
 </p>
 
 <div id="maze"></div>
+<div id="controls">
+
+<div>
+<button onclick="movePlayer(0,-1)">⬆️</button>
+</div>
+
+<div>
+<button onclick="movePlayer(-1,0)">⬅️</button>
+<button onclick="movePlayer(0,1)">⬇️</button>
+<button onclick="movePlayer(1,0)">➡️</button>
+</div>
+
+</div>
 
 <br>
 
